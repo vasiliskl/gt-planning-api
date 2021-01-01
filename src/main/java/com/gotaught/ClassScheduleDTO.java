77dee1f74e81472415cb34add0a15dbb0d12b133
vasiliskl <vasiliskl@vasiliskl-OptiPlex-9010>
2021-01-01 17:46:03 +0200

    Added class schedule resource

---
+++ src/main/java/com/gotaught/ClassScheduleDTO.java
@@ -0,0 +1,62 @@
+package com.gotaught;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Vasilis Kleanthous
+ * This class implements the input of the REST endpoints and represents the data coming from sensors.
+ *
+ */
+public class ClassScheduleDTO {
+
+	@NotNull
+	private Long sensorId;
+	@Digits(integer=2, fraction=6)
+	private Double latitude;
+	@Digits(integer=2, fraction=6)
+	private Double longitude;
+	@Digits(integer=2, fraction=2)
+	private Double humidity;
+	@Digits(integer=2, fraction=1)
+	private Double temperature;
+	@Digits(integer=2, fraction=1)
+	private Double pressure;
+	
+	public Long getSensorId() {
+		return sensorId;
+	}
+	public void setSensorId(Long sensorId) {
+		this.sensorId = sensorId;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public Double getHumidity() {
+		return humidity;
+	}
+	public void setHumidity(Double humidity) {
+		this.humidity = humidity;
+	}
+	public Double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(Double temperature) {
+		this.temperature = temperature;
+	}
+	public Double getPressure() {
+		return pressure;
+	}
+	public void setPressure(Double pressure) {
+		this.pressure = pressure;
+	}
+}
--- src/main/java/com/gotaught/ClassScheduleResource.java
+++ src/main/java/com/gotaught/ClassScheduleResource.java
@@ -5,8 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
-public class GreetingResource {
+@Path("/class-schedule")
+public class ClassScheduleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
